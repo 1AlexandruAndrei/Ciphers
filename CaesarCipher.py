d8554dfe6120e658a428044f23d55819dc9c703a
1AlexandruAndrei <andrei.alexandru.ro@gmail.com>
2024-04-05 23:44:20 +0300

    added the encoder

---
+++ CaesarCipher.py
@@ -0,0 +1,25 @@
+import sys
+
+def caesarEncryption(str, shift):
+    encrypted=""
+    for i in range(len(str)):
+        letter=str[i]
+        if letter.isupper():
+            encrypted+=chr((ord(letter)+shift-65)%26+65)
+        else:
+            encrypted+=chr((ord(letter)+shift-97)%26+97)
+    
+    return encrypted
+
+def main():
+    if len(sys.argv)!=3:
+        print("EXECUTE LIKE THIS: python3 CaesarCipher.py <your_string> <shift_value>")
+        return
+    
+    str=sys.argv[1]
+    shift=int(sys.argv[2])
+    print(caesarEncryption(str, shift))
+
+if __name__=="__main__":
+    main()
+    
